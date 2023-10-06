ba8fe487d49a3bd1583b5c7f78964e15f59ad80e
Milagro Scardigno <mdscardigno@gmail.com>
2023-10-05 21:08:46 -0400

    creating variables and asking user for info

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 import './style.css'
+
+const numberOfCupOfCoffee = 1
+const fullName = 'Mila Scardigno'
+console.log(
+  `My name is ${fullName} and I drink ${numberOfCupOfCoffee} cups of coffee a day.`
+)
+
+const aboutMe = {
+  fullName: 'Mila Scardigno',
+  luckyNumber: 7,
+  favoriteMovies: ['The Lord of the Rings', 'The Hobbit', 'Harry Potter'],
+}
+const userName = window.prompt('What is your name?')
